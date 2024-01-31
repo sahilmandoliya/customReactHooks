ffb1716d0f4dd1bce1b41ed66368705e7cd7d165
sahilmandoliya <mandoliyasahil@gmail.com>
2024-01-31 23:32:43 +0530

    useScrollProgress added

---
+++ src/utils/useScrollProgress.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react';
+
+const useScrollProgress = () => {
+  const [scrollProgress, setScrollProgress] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const windowHeight = window.innerHeight;
+      const documentHeight = document.documentElement.scrollHeight - windowHeight;
+      const scrollY = window.scrollY || window.pageYOffset;
+
+      const progress = scrollY / documentHeight;
+      setScrollProgress(progress);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return scrollProgress;
+};
+
+export default useScrollProgress;
